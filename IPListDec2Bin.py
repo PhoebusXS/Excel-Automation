50795e59060d2d0dfb996ae0b44151debdff3e1d
PhoebusXS <PhoebusXS@gmail.com>
2016-05-30 16:00:52 +0800

    IP List Conversion Dec to Bin

---
+++ IPListDec2Bin.py
@@ -0,0 +1,22 @@
+from openpyxl import load_workbook
+
+def ipDec2Bin(ip):
+	ansList = []
+	digList = ip.split('.')
+	for dig in digList:
+		binDig = bin(int(dig))
+		ansList.append(binDig[2:len(binDig)])
+	return '.'.join(ansList)
+
+wb = load_workbook('test.xlsx')
+ws = wb['Sheet 1']
+for col in ws.columns[0:1]:
+	for cell in col:
+		if cell.value:
+			try:
+				ipBin = ipDec2Bin(cell.value)
+				ws['B%d' % int(cell.coordinate[1])] = ipBin
+			except:
+				pass
+
+wb.save('resultIPList.xlsx')
